c9d6c7f9cf45b161b644250dcc39607f8d1a9d61
KhaelM <mikerandria@gmail.com>
2019-07-12 20:38:47 +0300

    %d changed %ld and explanation added

--- differenceBetweenTwoPointers/differenceBetweenTwoPointers.c
+++ differenceBetweenTwoPointers/differenceBetweenTwoPointers.c
@@ -7,13 +7,22 @@ int main(int argc, char const *argv[])
     printf("address = %p\n", addressAsInt);
     char* address2AsInt = (char*)  0x61FF14;
     printf("address2 = %p\n", address2AsInt);
-    printf("rest = %d\n", address2AsInt - addressAsInt);
+    printf("rest = %ld\n", address2AsInt - addressAsInt);
 
     int* address = (int*) 0x61FF08;
     printf("address = %p\n", address);
     int* address2 = (int*)  0x61FF14;
     printf("address2 = %p\n", address2);
-    printf("rest = %d\n", address2 - address);
+    printf("rest = %ld\n", address2 - address);
+    /*
+    output: 3
+    Explanation:
+    0x61FF08 to 0x61FF0B => 1 integer
+    0x61FF0C to 0x61FF0F => 1 integer
+    0x61FF10 to 0x61FF13 => 1 integer
+    0x61FF14 is excluded
+    its [0x61FF08; 0x61FF14[
+     */    
 
     return 0;
 }
